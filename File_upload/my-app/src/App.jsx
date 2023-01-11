978580f6fd0b5260746e97c413cf46e6e56b5037
Quist,Wendela W.F <w.quist@student.fontys.nl>
2023-01-11 12:36:45 +0100

    added style

--- File_upload/my-app/src/App.jsx
+++ File_upload/my-app/src/App.jsx
@@ -11,16 +11,25 @@ function App() {
     const fileRef = ref(storage, `uploads/${fileUpload.name + v4()}`);
     uploadBytes(fileRef, fileUpload).then(() => {
       alert("File good to go");
-    })
+    })//return sollution???
 
   };
 
   return <div className="App">
+    <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    </head>
+    <body>
+    <div class="drag-area">
+    <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
+    <header><h1>File Upload</h1></header>
     <input type="file" onChange={(event) => {
       setFileUpload(event.target.files[0])
     }}
     />
     <button onClick={uploadFile}> Upload File </button>
+    </div>
+    </body>
     </div>
 }
 
---
+++ File_upload/my-app/src/FileUpload/FileUpload.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const fileUpload = ({files, setFiles}) => {
+    const uploadHandler = () => {}
+    return (
+        <>
+        <div className="file-card">
+            <div className="file-inputs">
+                <input type="files" />
+            </div>
+        </div>
+        </>
+    )
+}
